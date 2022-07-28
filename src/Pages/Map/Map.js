a5b72a67cf96fed44ba1c0578f70453172cd7d5d
OybekAbduljabbor <abdujabborovoybek@gmail.com>
2022-07-28 17:10:01 +0500

    fix

--- src/Pages/Map/Map.js
+++ src/Pages/Map/Map.js
@@ -7,9 +7,9 @@ export function Map() {
       <iframe
         style={myStyle.myMap}
         src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d20257.48579824117!2d71.65573135441623!3d40.996952232828086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1659009200454!5m2!1sru!2s"
-        allowfullscreen=""
+        allowFullScreen=""
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
+        referrerPolicy="no-referrer-when-downgrade"
         title="This is a unique title"
       />
     </Box>
